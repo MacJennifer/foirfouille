b17b22c16e9c92fd1894d85dcdb52879c8fa8290
Jennifer Macaire <macaire.jennifer@sfr.fr>
2024-01-09 17:47:01 +0100

    premier commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+
+use App\Models\Categorie;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'image' => $this->faker->imageUrl(),
+            'description' => $this->faker->paragraph,
+            'price' => $this->faker->randomFloat(2, 10, 100),
+            'categorie_id' => function () {
+                return Categorie::factory()->create()->id;
+            },
+        ];
+    }
+}
